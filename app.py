4ded91be8adbca0366937c3f8bee35cd700dbad0
Joao Vitor <joaovrsilveira16@gmail.com>
2025-02-05 11:22:14 -0300

    Update para usar django futuramente

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 import flet as ft
-from models import Produto
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-
-# conexao com o db
-CONN = 'sqlite:///projeto2.db'
-
-engine = create_engine(CONN, echo = True)
-Session = sessionmaker(bind=engine)
-session = Session()
+import requests
 
 # funcao alvo para a renderizacao
 def main(page: ft.Page):
@@ -16,18 +7,28 @@ def main(page: ft.Page):
     lista_produtos = ft.ListView()
 
     def cadastrar(event):
-        try:
-            novo_produto = Produto(titulo=produto.value, preco=preco.value)
-            session.add(novo_produto)
-            session.commit()
-            lista_produtos.controls.append(ft.Container(ft.Text(produto.value), bgcolor=ft.colors.BLACK12, padding=15, alignment=ft.alignment.center, margin=3, border_radius=3))
-            txt_erro.visible = False
-            txt_acerto.visible = True
-        except:
-            txt_erro.visible = True
-            txt_acerto.visible = False
+        data = {
+            'nome': produto.value,
+            'preco': preco.value,
+            'categoria_id': 1
 
+        }
+
+        response = requests.post('http://127.0.0.1:8000/produtos/produto/', json=data)
+
+        if response.status_code == 200:
+            lista_produtos.controls.append(
+                ft.Container(
+                    ft.Text(produto.value),
+                    bgcolor=ft.colors.BLACK12,
+                    padding=15,
+                    alignment=ft.alignment.center,
+                    margin=3,
+                    border_radius=10
+                )
+            )
         page.update()
+            
 
     txt_erro = ft.Container(ft.Text('Erro ao salvar o produto'), visible=False, bgcolor=ft.colors.RED, padding=10, alignment=ft.alignment.center)
     txt_acerto = ft.Container(ft.Text('Produto salvo com sucesso'), visible=False, bgcolor=ft.colors.GREEN, padding=10, alignment=ft.alignment.center)
@@ -50,8 +51,6 @@ def main(page: ft.Page):
         txt_erro
     )
 
-    for p in session.query(Produto).all():
-        lista_produtos.controls.append(ft.Container(ft.Text(p.titulo), bgcolor=ft.colors.BLACK12, padding=15, alignment=ft.alignment.center, margin=3, border_radius=3))
 
     page.add(
         lista_produtos
